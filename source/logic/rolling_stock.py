41a166d8f18eca9937c3378a42db4282a444dfb1
Collin Barker <97cweb@gmail.com>
2026-07-01 17:26:46 -0400

    genericify services for coupling

--- source/logic/rolling_stock.py
+++ source/logic/rolling_stock.py
@@ -1,4 +1,5 @@
 from __future__ import annotations
+from _typeshed import DataclassInstance
 from dataclasses import dataclass, field
 
 from source.logic.types import Float
@@ -14,7 +15,6 @@ class RollingStock:
     condition: Float
     handbrake: bool
 
-
     rolling_resistance: Float
     automatic_brake_force: Float
     max_automatic_brake_force: Float
@@ -25,6 +25,8 @@ class RollingStock:
 
     front_connection: Connection
     rear_connection: Connection
+
+    trucks: list[Truck] = field(default_factory=list)
 
     @property
     def mass(self) -> Float:
@@ -47,11 +49,11 @@ class Caboose(Car):
 
 @dataclass
 class Tender(RollingStock):
-    max_fuel_mass: Float
-    max_water_mass: Float
-
     fuel_mass: Float = 0.0
     water_mass: Float = 0.0
+
+    max_fuel_mass: Float = 0.0
+    max_water_mass: Float = 0.0
 
     @property
     def mass(self) -> Float:
@@ -60,30 +62,69 @@ class Tender(RollingStock):
 
 @dataclass
 class Locomotive(RollingStock):
-    tractive_effort: Float
+    tractive_effort: Float = 0.0
     tender: Tender | None = None
     independent_brake_force: Float = 0.0
     max_independent_brake_force: Float = 0.0
 
 
 @dataclass
+class ServiceConnection:
+    connected: bool = False
+
+
+@dataclass
+class BrakeLine(ServiceConnection):
+    valve_a_open: bool = False
+    valve_b_open: bool = False
+
+
+@dataclass
+class SteamLine(ServiceConnection):
+    valve_a_open: bool = False
+    valve_b_open: bool = False
+
+
+@dataclass
+class ElectricalJumper(ServiceConnection):
+    voltage: Float = 0.0
+    breaker_closed: bool = False
+
+
+@dataclass
+class MUJumper(ServiceConnection):
+    connected: bool = False
+
+
+@dataclass
 class Connection:
     a: RollingStock | None = None
     b: RollingStock | None = None
-    brake_hose_connected: bool = False
-    angle_cock_a_open: bool = False
-    angle_cock_b_open: bool = False
+    slack: Float = 0.025  # m
+    position: Float = 0.0
+    max_tension_force: Float = 0.0
+    max_compression_force: Float = 0.0
+    a_knuckle_open: bool = True  # whether connections are open. knuckle open or not
+    b_knucle_open: bool = True  # whether connections are open. knuckle open or not
+    brakeline: BrakeLine | None = None
+    steam: SteamLine | None = None
+    electrical: ElectricalJumper | None = None
+    mu: MUJumper | None = None
 
     @property
     def can_uncouple(self) -> bool:
         return False
 
-    @property brake_pipe_continuous(self) -> bool:
-        return (
-            self.brake_hose_connected
-            and self.angle_cock_a_open
-            and self.angle_cock_b_open
-        )
+    @property
+    def brake_pipe_continuous(self) -> bool:
+        if self.brakeline:
+            return (
+                self.brakeline.connected
+                and self.brakeline.valve_a_open
+                and self.brakeline.valve_b_open
+            )
+        else:
+            return False
 
 
 @dataclass
@@ -98,3 +139,14 @@ class FixedConnection(Connection):
     @property
     def can_uncouple(self) -> bool:
         return False
+
+
+@dataclass
+class Truck:
+    offset: Float
+    wheelbase: Float
+    axle_count: int
+
+    can_swivel: bool = True
+    swivel_angle: Float | None = None
+    max_swivel_angle: Float | None = None
